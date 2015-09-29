c0411aa74d68a5d47c320e1faede1b2775d32aca
Andrew Newton <andy@arin.net>
2015-09-28 20:19:59 -0500

    started on evaluating rules

---
+++ lib/jcr/evaluate_rules.rb
@@ -0,0 +1,80 @@
+# Copyright (c) 2015 American Registry for Internet Numbers
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+# IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+require 'jcr/parser'
+require 'jcr/map_rule_names'
+require 'jcr/check_groups'
+
+module JCR
+
+  class Evaluation
+    attr_accessor :success, :reason
+    def initialize success, reason
+      @success = success
+      @reason = reason
+    end
+  end
+
+  def self.evaluate_rule jcr, rule_atom, data, mapping
+    case
+      when jcr[:rule]
+        return evaluate_rule( jcr[:rule], rule_atom, data, mapping)
+      when jcr[:value_rule]
+        return evaluate_value_rule( jcr[:value_rule], rule_atom, data, mapping)
+      when jcr[:group_rule]
+        return evaluate_group_rule( jcr[:group_rule], rule_atom, data, mapping)
+      else
+        return Evaluation.new( true, nil )
+    end
+  end
+
+  def self.evaluate_value_rule jcr, rule_atom, data, mapping
+    case
+      when jcr[:any]
+        return Evaluation.new( true, nil )
+      when jcr[:integer_v]
+        si = jcr[:integer_v].to_s
+        if si == "integer"
+          return bad_value( jcr, rule_atom, "integer", data ) unless data.is_a?( Fixnum )
+        end
+      when jcr[:integer]
+        i = jcr[:integer_v].to_s.to_i
+        return bad_value( jcr, rule_atom, i, data ) unless data == i
+      when jcr[:integer_min],jcr[:integer_max]
+        min = jcr[:integer_min].to_s.to_i
+        return bad_value( jcr, rule_atom, min, data ) unless data >= min
+        max = jcr[:integer_max].to_s.to_i
+        return bad_value( jcr, rule_atom, max, data ) unless data <= max
+      when jcr[:string]
+        return bad_value( jcr, rule_atom, "string", data ) unless data.is_a? String
+      when jcr[:q_string]
+        s = jcr[:q_string].to_s
+        return bad_value( jcr, rule_atom, s, data ) unless data == s
+      when jcr[:group_rule]
+        return evaluate_group_rule jcr[:group_rule], rule_atom, data, mapping
+      else
+        return Evaluation.new( true, nil )
+    end
+    return Evaluation.new( true, nil )
+  end
+
+  def self.bad_value jcr, rule_atom, expected, actual
+    Evaluation.new( false, "expected #{expected} but got #{actual} at #{jcr} from #{rule_atom}" )
+  end
+
+  def self.evaluate_group_rule jcr, rule_atom, data, mapping
+    return Evaluation.new( true, nil )
+  end
+
+end
---
+++ spec/evaluate_rules_spec.rb
@@ -0,0 +1,108 @@
+# Copyright (C) 2015 American Registry for Internet Numbers (ARIN)
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+# IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+require 'rspec'
+require 'pp'
+require_relative '../lib/JCR/evaluate_rules'
+
+describe 'evaluate_rules' do
+
+  it 'should pass a string constant' do
+    tree = JCR.parse( 'trule : "a string constant"' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], "a string constant", mapping )
+    expect( e.success ).to be_truthy
+  end
+
+  it 'should fail a strng constant' do
+    tree = JCR.parse( 'trule : "a string constant"' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], "another string constant", mapping )
+    expect( e.success ).to be_falsey
+  end
+
+  it 'should pass a string variable' do
+    tree = JCR.parse( 'trule : string' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], "a string constant", mapping )
+    expect( e.success ).to be_truthy
+  end
+
+  it 'should fail a string variable defined as a constant' do
+    tree = JCR.parse( 'trule : "a string constant"' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], "another string constant", mapping )
+    expect( e.success ).to be_falsey
+  end
+
+  it 'should pass an integer variable' do
+    tree = JCR.parse( 'trule : integer' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], 2, mapping )
+    expect( e.success ).to be_truthy
+  end
+
+  it 'should fail an integer variable defined as a constant' do
+    tree = JCR.parse( 'trule : 3' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], 2, mapping )
+    expect( e.success ).to be_falsey
+  end
+
+  it 'should pass an integer within a range' do
+    tree = JCR.parse( 'trule : 1..3' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], 2, mapping )
+    expect( e.success ).to be_truthy
+  end
+
+  it 'should fail an integer below a range' do
+    tree = JCR.parse( 'trule : 1..3' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], 0, mapping )
+    expect( e.success ).to be_falsey
+  end
+
+  it 'should fail an integer above a range' do
+    tree = JCR.parse( 'trule : 1..3' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], 4, mapping )
+    expect( e.success ).to be_falsey
+  end
+
+  it 'should pass an integer at the min of a range' do
+    tree = JCR.parse( 'trule : 1..3' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], 1, mapping )
+    expect( e.success ).to be_truthy
+  end
+
+  it 'should pass an integer at the max of a range' do
+    tree = JCR.parse( 'trule : 1..3' )
+    mapping = JCR.map_rule_names( tree )
+    JCR.check_rule_target_names( tree, mapping )
+    e = JCR.evaluate_rule( tree[0], tree[0], 3, mapping )
+    expect( e.success ).to be_truthy
+  end
+
+end
