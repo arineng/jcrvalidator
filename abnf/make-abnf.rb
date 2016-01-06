d26e30353f5352e40e617ff343e5b3235494c80f
Pete Cordell <git@codalogic.com>
2016-01-06 13:35:07 +0000

    Finished Ruby ABNF extractor

--- abnf/make-abnf.rb
+++ abnf/make-abnf.rb
@@ -14,15 +14,15 @@
 
 # Configuration
 $parslet_src_file = "../lib/jcr/parser.rb"
-$abnf_out_file = "jcr-abnf-rb.txt"
+$abnf_out_file = "jcr-abnf.txt"
 $margin = 0
 $indent = 19
 $keyword_indent = 30
 
 # Primary data stores
-$mappings = Hash.new
+$mappings = {}
 $lines = []
-$keywords = Hash.new
+$keywords = {}
 
 def main
     read_parslet_def
@@ -30,7 +30,7 @@ def main
 end
 
 def read_parslet_def
-    IO.foreach( $parslet_src_file ) { |line|
+    File.foreach( $parslet_src_file ) { |line|
         conditionally_grab_mapping( line )
         conditionally_grab_abnf( line )
         conditionally_grab_keywords( line )
@@ -56,7 +56,7 @@ def conditionally_grab_keywords( line )
     # Example haystack:  #> jcr-version-kw = "jcr-version"
     if m = /^\s*#>\s*([\w_\-]+kw)(\s*=\s*)"([^"]+)"/.match( line )
         name, equals, keyword = m.captures
-        ascii_codes = keyword.chars.map{ |c| sprintf( "%02X", c.ord ) }.join( '.' )
+        ascii_codes = keyword.chars.map{ |c| "%02X" % c.ord }.join( '.' )
         comment_padding = ' ' * [($keyword_indent - 3*keyword.length), 0].max
         line = name + equals + '%x' + ascii_codes + comment_padding + ' ; "' + keyword + '"'
         $keywords[name] = line
@@ -64,7 +64,94 @@ def conditionally_grab_keywords( line )
 end
 
 def write_abnf
-    puts "Shall be writing to #{$abnf_out_file}!"
+    File.open( $abnf_out_file, 'w' ) { |fout|
+        print_captured_abnf( fout )
+        print_keywords( fout )
+        print_referenced_rfc5234_rules( fout )
+    }
+end
+
+def print_captured_abnf( fout )
+    for line in $lines
+        line = do_name_mappings( line )
+        pretty_print( line, fout )
+    end
+end
+
+def do_name_mappings( line )
+    for mapping_from in mapping_keys_sorted_longest_first
+        line.gsub!( mapping_from, $mappings[mapping_from] )
+    end
+    line.gsub!( '_', '-' )
+    return line
+end
+
+def mapping_keys_sorted_longest_first
+    return $mappings.keys.sort_by { |m| m.length }.reverse
+end
+
+def print_keywords( fout )
+    pretty_print( '', fout )
+    pretty_print( ';; Keywords', fout )
+    for kw in $keywords.keys().sort
+        pretty_print( $keywords[kw], fout )
+    end
+end
+
+def print_referenced_rfc5234_rules( fout )
+    rfc5234_core_rules = [
+        'ALPHA          =  %x41-5A / %x61-7A   ; A-Z / a-z',
+        'CR             =  %x0D         ; carriage return',
+        'DIGIT          =  %x30-39      ; 0-9',
+        # 'DQUOTE         =  %x22         ; " (Double Quote)',
+        'HEXDIG         =  DIGIT / "A" / "B" / "C" / "D" / "E" / "F"',
+        'HTAB           =  %x09         ; horizontal tab',
+        'LF             =  %x0A         ; linefeed',
+        'SP             =  %x20         ; space',
+        'WSP            =  SP / HTAB    ; white space'
+        ]
+    pretty_print( '', fout )
+    pretty_print( ';; Referenced RFC 5234 Core Rules', fout )
+    for rule in rfc5234_core_rules
+        pretty_print( rule, fout )
+    end
+end
+
+def pretty_print( line, fout )
+    if is_blank_line?( line )
+        fout.write( "\n" )
+    else
+        fout.write( ' ' * $margin )
+        if is_block_comment?( line )
+            fout.write( line + "\n" )
+        elsif comment = local_comment( line )
+            fout.write( ' ' * $indent + comment + "\n" )
+        elsif mapping = abnf_mapping( line )
+            name, expansion = mapping
+            equals = ' = '
+            padding_needed = [ $indent - (name.length + equals.length), 0 ].max
+            padding = ' ' * padding_needed
+            fout.write( name + padding + equals + expansion + "\n" )
+        else
+            fout.write( ' ' * $indent + line + "\n" ) # A 2nd/3rd expression line
+        end
+    end
+end
+
+def is_blank_line?( line )
+    return /^\s*$/.match( line )
+end
+
+def is_block_comment?( line )
+    return /^\s*;;/.match( line )
+end
+
+def local_comment( line )
+    return (m = /^\s*(;.*)/.match( line )) ? m[1] : false
+end
+
+def abnf_mapping( line )
+    return (m = /([\w_\-]+)\s*=\s*(.*)/.match( line )) ? [m[1], m[2]] : false
 end
 
 main
